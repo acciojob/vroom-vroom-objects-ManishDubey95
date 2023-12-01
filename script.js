052d050bfc978ec11054aa36906bfe8db5e4348f
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-12-02 00:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ function Car(make, model) {
     this.make = make;
     this.model = model;
 }
-Car.prototype.getMakeMode = function(){
+
+Car.prototype.getMakeModel = function() {
     return `${this.make} ${this.model}`;
-}
+};
 
 function SportsCar(make, model, topSpeed) {
-    this.topSpeed = top;
-    SportsCar.call(this, make, model);
+    // Call the Car constructor to initialize make and model
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype.__proto__ = Car.prototype;
+// Set up the prototype chain
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Set the constructor property for correct instanceof checks
+SportsCar.prototype.constructor = SportsCar;
 
 // Do not change the code below
 window.Car = Car;
