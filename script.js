8667dca488c26a7c7380b157cd31671b42ed4212
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-12-01 23:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
+Car.prototype.getMakeMode = function(){
+    return `${this.make} ${this.model}`;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+    this.topSpeed = top;
+    SportsCar.call(this, make, model);
+}
+
+SportsCar.prototype.__proto__ = Car.prototype;
 
 // Do not change the code below
 window.Car = Car;
